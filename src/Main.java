1a8a48c6e99f67de70e76702bc13ed29cd288873
98munozpatricia <43338941+98munozpatricia@users.noreply.github.com>
2018-09-21 10:54:09 +0200

    Update main vector

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         double areatotal=0;
-        Figura figura = new Figura();
-        double Area = figura.CalculArea("Rectangle", 4,5,0 );
+        Figura figura [] = new Figura[10];
+        double[] Area = new double[10];
+        for (int i=0; i<10; i++)
+        {
+            Area[0] = figura[i].CalculArea("Rectangle", 4,5,0 );
+            System.out.println("Area de la figura:");
+            System.out.println (Area[i]);
+        }
+        for (int i=0; i<10; i++)
+        {
+            areatotal=areatotal + Area[i];
+        }
 
         System.out.println("Area de la figura:");
-        System.out.println (Area);
-        areatotal=areatotal+Area;
-        Area = figura.CalculArea("Rectangle", 4,5,0 );
-        System.out.println("Area de la figura:");
-        System.out.println (Area);
-        areatotal=areatotal+Area;
-        Area = figura.CalculArea("Rectangle", 4,5,0 );
-        System.out.println("Area de la figura:");
-        System.out.println (Area);
-        areatotal=areatotal+Area;
-        System.out.println("Area total: "+areatotal);
+        System.out.println (areatotal);
+
 
     }
 
